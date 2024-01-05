918582cdc88a63074d3b68facea13e6f2fd48a39
biruk1122 <birukfikadu29@gmail.com>
2024-01-05 10:05:43 -0800

    updated

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -7,17 +7,15 @@
  * @tree: Pointer to the root node of the tree to traverse
  * @func: Pointer to a function to call for each node
  *
- * Description: If tree or func is NULL, do nothing.
+ * Description: The value in the node must be passed as a parameter to this function.
+ * If tree or func is NULL, do nothing.
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
     if (tree == NULL || func == NULL)
         return;
     binary_tree_t **queue = malloc(sizeof(binary_tree_t *) * 1024);
-    size_t front = 0, rear = 0;
-
-    if (queue == NULL)
-        return;
+    int front = 0, rear = 0;
 
     queue[rear++] = (binary_tree_t *)tree;
 
@@ -25,13 +23,11 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
     {
         binary_tree_t *current = queue[front++];
         func(current->n);
-
         if (current->left != NULL)
             queue[rear++] = current->left;
 
         if (current->right != NULL)
             queue[rear++] = current->right;
     }
-
     free(queue);
 }
